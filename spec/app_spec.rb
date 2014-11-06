3621ef3ff9002aab3277cd329955aad514f979ed
CliveIMPISA <augustinclive@hotmail.com>
2014-11-07 07:44:01 +0800

    Some test written

---
+++ spec/app_spec.rb
@@ -0,0 +1,80 @@
+require_relative 'spec_helper'
+require_relative 'support/story_helpers'
+require 'json'
+
+describe 'TeamPay Stories' do
+  include StoryHelpers
+
+  describe 'Getting the root of the TeamPay Service' do
+    it 'Should return ok' do
+      get '/'
+      last_response.must_be :ok?
+      last_response.body.must_match(/welcome/i)
+    end
+  end
+
+  describe 'Getting Team information' do
+    it 'should return their Salary Information' do
+      get '/api/v1/MIA.json'
+      last_response.must_be :ok?
+    end
+      it 'should return 404 for unknown user' do
+      get "/api/v1/#{random_str(20)}.json"
+      last_response.must_be :not_found?
+    end
+  end
+
+  describe 'Checking specified player or players\' salary' do
+    it 'should return of two Phoenix players\' salaries' do
+      header = { 'CONTENT_TYPE' => 'application/json' }
+      body = {
+        teamname: ["PHO"],
+        player_name: ["Archie Goodwin", "Marcus Morris"]
+        }
+      post '/api/v1/check', body.to_json, header
+      last_response.must_be :ok?
+    end
+    it 'should return 404 for unknown players' do
+      header = { 'CONTENT_TYPE' => 'application/json' }
+      body = {
+      teamname: [random_str(15),
+      player_name: [random_str(30)]
+      }
+      post '/api/v1/check', body.to_json, header
+      last_response.must_be :not_found?
+    end
+    it 'should return 400 for bad JSON formatting' do
+      header = { 'CONTENT_TYPE' => 'application/json' }
+      body = random_str(50)
+      post '/api/v1/check', body, header
+      last_response.must_be :bad_request?
+    end
+  end
+
+  describe 'Checking users for badges' do
+    it 'should find missing badges' do
+      header = { 'CONTENT_TYPE' => 'application/json' }
+      body = {
+        teamname: ["PHO"],
+        player_name: ["Archie Goodwin", "Marcus Morris"]
+        }
+      post '/api/v1/check2', body.to_json, header
+      last_response.must_be :ok?
+    end
+    it 'should return 404 for unknown users' do
+      header = { 'CONTENT_TYPE' => 'application/json' }
+      body = {
+      teamname: [random_str(15),
+      player_name: [random_str(30)]
+      }
+      post '/api/v1/check2', body.to_json, header
+      last_response.must_be :not_found?
+    end
+    it 'should return 400 for bad JSON formatting' do
+      header = { 'CONTENT_TYPE' => 'application/json' }
+      body = random_str(50)
+      post '/api/v1/check2', body, header
+      last_response.must_be :bad_request?
+    end
+  end
+end
