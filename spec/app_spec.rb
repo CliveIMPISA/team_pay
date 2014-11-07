7a4386adddc07bca6f3552bf10c41cda34108a41
CliveIMPISA <augustinclive@hotmail.com>
2014-11-07 13:18:40 +0800

    Some code to past tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -18,7 +18,7 @@ describe 'TeamPay Stories' do
       get '/api/v1/MIA.json'
       last_response.must_be :ok?
     end
-    it 'should return 404 for unknown user' do
+    it 'should return 404 for unknown team' do
       get "/api/v1/#{random_str(20)}.json"
       last_response.must_be :not_found?
     end
@@ -27,20 +27,16 @@ describe 'TeamPay Stories' do
   describe 'TeamPayApp' do
     it "should return of two Phoenix players' salaries" do
       header = { 'CONTENT_TYPE' => 'application/json' }
-      body = {
-        teamname: ['PHO'],
-        player_name: ['Archie Goodwin', 'Marcus Morris']
-             }
+      body = { teamname: ['PHO'],
+               player_name: ['Archie Goodwin', 'Marcus Morris'] }
       post '/api/v1/check', body.to_json, header
       last_response.must_be :ok?
     end
 
     it 'should return 404 for unknown players' do
       header = { 'CONTENT_TYPE' => 'application/json' }
-      body = {
-        teamname: [random_str(15)],
-        player_name: [random_str(30)]
-             }
+      body = { teamname: [random_str(15)],
+               player_name: [random_str(30)] }
       post '/api/v1/check', body.to_json, header
       last_response.must_be :not_found?
     end
@@ -56,19 +52,14 @@ describe 'TeamPay Stories' do
   describe "should return of two Phoenix players' salaries" do
     it 'should find total salaries of the two players below' do
       header = { 'CONTENT_TYPE' => 'application/json' }
-      body =  {
-        teamname: ['PHO'],
-        player_name: ['Archie Goodwin', 'Marcus Morris']
-              }
+      body =  { teamname: ['PHO'],
+                player_name: ['Archie Goodwin', 'Marcus Morris'] }
       post '/api/v1/check2', body.to_json, header
       last_response.must_be :ok?
     end
     it 'should return 404 for unknown users' do
       header = { 'CONTENT_TYPE' => 'application/json' }
-      body = {
-        teamname: [random_str(15)],
-        player_name: [random_str(30)]
-      }
+      body = { teamname: [random_str(15)], player_name: [random_str(30)] }
       post '/api/v1/check2', body.to_json, header
       last_response.must_be :not_found?
     end
