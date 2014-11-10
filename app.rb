945c7c2b95adbe14d7327a9f44ef400c220f2317
CliveIMPISA <augustinclive@hotmail.com>
2014-11-10 11:06:55 +0800

    testing new method

--- app.rb
+++ app.rb
@@ -1,13 +1,10 @@
 require 'sinatra/base'
-require 'sinatra/namespace'
 require 'sinatra'
 require 'nbasalaryscrape'
 require 'json'
 
 # nbasalaryscrape service
 class TeamPayApp < Sinatra::Base
-  register Sinatra::Namespace
-
   helpers do
     def get_team(teamname)
       var = SalaryScraper::BasketballReference.new
@@ -81,52 +78,48 @@ class TeamPayApp < Sinatra::Base
       money = "$#{data.to_s.reverse.gsub(/...(?=.)/, '\&,').reverse}"
       money
     end
-  end
-
-  namespace '/api/v1'  do
-    get '/:teamname.json' do
-      content_type :json
-      get_team(params[:teamname]).to_json
-    end
-
-    get '/form' do
-      erb :form
-    end
-
-    post '/check' do
-      content_type :json
-      begin
-        req = JSON.parse(request.body.read)
-      rescue
-        halt 400
+    ###########################################################################
+    def two_players_salary_data(teamname, player_name)
+      salary_scrape = get_team(teamname[0])
+      diff=0
+      player_scrape = []
+      player_name.each do |each_player|
+        salary_scrape.each do |data_row|
+          player_scrape << diff_total(data_row, each_player) if data_row['Player'] == each_player
+        end
       end
-      teamname = req['teamname']
-      player_name = req['player_name']
-      player_salary_data(teamname, player_name).to_json
-    end
-    
-    post '/check2' do
-      content_type :json
-      begin
-        req = JSON.parse(request.body.read)
-      rescue
-        halt 400
+      if player_scrape[0]['fullpay'] > player_scrap[1]['fullpay']
+        diff= player_scrape[0]['fullpay'] - player_scrap[1]['fullpay']
+        full_pay << {'fullpay' => back_to_money(playerscrape)}
+      else
+        diff= player_scrap[1]['fullpay'] - player_scrape[0]['fullpay']
+        full_pay << {'fullpay' => back_to_money(playerscrape)}
       end
-      teamname = req['teamname']
-      player_name = req['player_name']
-      player_total_salary(teamname, player_name).to_json
+      diff
     end
 
-    get '/players/:teamname.json' do
-      content_type :json
-      get_team_players(params[:teamname]).to_json
+    def diff_total(data_row, each_player)
+      player_scrape, fullpay = 0, []
+      player_scrape += parse_money(data_row['2014-15'])
+      player_scrape += parse_money(data_row['2015-16'])
+      player_scrape += parse_money(data_row['2016-17'])
+      player_scrape += parse_money(data_row['2017-18'])
+      player_scrape += parse_money(data_row['2018-19'])
+      player_scrape += parse_money(data_row['2019-20'])
+      fullpay = { 'player' => each_player,
+      'fullpay' => player_scrape }
+      fullpay
     end
+    ###########################################################################
   end
 
-  get '/' do
-    erb :index
+  get '/api/v1/:teamname.json' do
+    content_type :json
+    get_team(params[:teamname]).to_json
   end
-
+  get '/api/v1/form' do
+    erb :form
+  end
   post '/form' do
     content_type :json
     get_team(params[:message]).to_json
@@ -136,4 +129,49 @@ class TeamPayApp < Sinatra::Base
     status 404
     'not found'
   end
+
+  post '/api/v1/check' do
+    content_type :json
+    begin
+      req = JSON.parse(request.body.read)
+    rescue
+      halt 400
+    end
+    teamname = req['teamname']
+    player_name = req['player_name']
+    player_salary_data(teamname, player_name).to_json
+  end
+
+  post '/api/v1/check2' do
+    content_type :json
+    begin
+      req = JSON.parse(request.body.read)
+    rescue
+      halt 400
+    end
+    teamname = req['teamname']
+    player_name = req['player_name']
+    player_total_salary(teamname, player_name).to_json
+  end
+
+  get '/api/v1/players/:teamname.json' do
+    content_type :json
+    get_team_players(params[:teamname]).to_json
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  ###########################################################
+  post '/api/v1/check3' do
+    content_type :json
+    req = JSON.parse(request.body.read)
+    teamname = req['teamname']
+    player_name = req['player_name']
+    two_players_salary_data(teamname, player_name).to_json
+  end
+  ###########################################################
+
+
 end
